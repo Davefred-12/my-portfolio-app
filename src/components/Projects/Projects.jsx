994a9904f4ebce40498182a816f632740752784d
Davefred-12 <adeleyepamilerin9@gmail.com>
2024-09-24 16:13:34 +0100

    contact form changed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,13 +5,6 @@ import "./Projects.css";
 
 const projects = [
   {
-    name: "To-Do List App",
-    description:
-      "A straightforward application that helps users efficiently manage tasks and to-dos. With a clean design and intuitive functionality, this app enables users to stay organized and prioritize their daily activities effortlessly.",
-    demoLink: "https://todo-list-app-i5ik.onrender.com",
-    image: "/src/assets/Images/todo.png",
-  },
-  {
     name: "Dale Wears Ecommerce Website",
     description:
       "An innovative ecommerce platform designed for clothing and accessories, offering a seamless shopping experience. Users can explore a diverse range of products and enjoy a user-friendly interface to enhance their online shopping journey.",
@@ -24,6 +17,27 @@ const projects = [
       "A professional platform tailored for the engineering sector, offering solutions for project management and collaboration. This site connects users with essential resources and services to streamline their engineering projects..",
     demoLink: "https://qmoilandgas.com",
     image: "/src/assets/Images/cog.png",
+  },
+  {
+    name: "GetLinked Technologies",
+    description:
+      "An engaging application focused on task management, providing users with tools to keep track of their tasks effectively. Its simple interface ensures that users can prioritize and manage their responsibilities with ease.",
+    demoLink: "",
+    image: "/src/assets/Images/getLinked.png",
+  },
+  {
+    name: "Tech Alpha Quiz ",
+    description:
+      "A dynamic and interactive quiz platform designed to challenge and enhance your tech knowledge. With a focus on cutting-edge topics and fun gameplay, it's perfect for tech enthusiasts looking to test their skills and learn something new.",
+    demoLink: "https://quiz-game-app-6qbj.onrender.com",
+    image: "/src/assets/Images/alpha.png",
+  },
+  {
+    name: "Movie Search App ",
+    description:
+      "A professional platform tailored for the engineering sector, offering solutions for project management and collaboration. This site connects users with essential resources and services to streamline their engineering projects..",
+    demoLink: "https://deluxe-panda-13911d.netlify.app",
+    image: "/src/assets/Images/movie.png",
   },
   {
     name: "NCCF Etinan ",
@@ -40,32 +54,18 @@ const projects = [
     image: "/src/assets/Images/dice.png",
   },
   {
-    name: "Movie Search App ",
-    description:
-      "A professional platform tailored for the engineering sector, offering solutions for project management and collaboration. This site connects users with essential resources and services to streamline their engineering projects..",
-    demoLink: "https://deluxe-panda-13911d.netlify.app",
-    image: "/src/assets/Images/movie.png",
-  },
-  {
-    name: "Tech Alpha Quiz ",
-    description:
-      "A dynamic and interactive quiz platform designed to challenge and enhance your tech knowledge. With a focus on cutting-edge topics and fun gameplay, it's perfect for tech enthusiasts looking to test their skills and learn something new.",
-    demoLink: "https://quiz-game-app-6qbj.onrender.com",
-    image: "/src/assets/Images/alpha.png",
-  },
-  {
-    name: "GetLinked Technologies",
-    description:
-      "An engaging application focused on task management, providing users with tools to keep track of their tasks effectively. Its simple interface ensures that users can prioritize and manage their responsibilities with ease.",
-    demoLink: "",
-    image: "/src/assets/Images/getLinked.png",
-  },
-  {
     name: "Jokes and Word App",
     description:
       "Users can get random jokes with the click of a button, bringing laughter to their day. Additionally, the app allows users to enter words phonetics, and examples, making it a fun and educational tool. With a sleek design and engaging features, this app enhances learning and entertainment simultaneously.",
     demoLink: "https://joke-app-btkm.onrender.com",
     image: "/src/assets/Images/jokes.png",
+  },
+  {
+    name: "To-Do List App",
+    description:
+      "A straightforward application that helps users efficiently manage tasks and to-dos. With a clean design and intuitive functionality, this app enables users to stay organized and prioritize their daily activities effortlessly.",
+    demoLink: "https://todo-list-app-i5ik.onrender.com",
+    image: "/src/assets/Images/todo.png",
   },
 ];
 const Projects = () => {
