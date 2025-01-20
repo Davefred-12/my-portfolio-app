50e0ab7deabda6d8d4e2ba7ad2af5e1e3951b630
Davefred-12 <adeleyepamilerin9@gmail.com>
2025-01-20 12:43:16 +0100

    new project added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,57 +6,69 @@ import "./Projects.css";
 const projects = [
   {
     name: "Dale Wears Ecommerce Website",
-    description: "An innovative ecommerce platform designed for clothing and accessories.",
+    description:
+      "An innovative ecommerce platform designed for clothing and accessories.",
     demoLink: "https://dalewears.onrender.com",
-    image: '/images/dala.png', // Direct URL from public folder
+    image: "/images/dala.png", // Direct URL from public folder
   },
   {
     name: "Foresite Engineering",
     description: "A professional platform tailored for the engineering sector.",
     demoLink: "https://qmoilandgas.com",
-    image: '/images/cog.png',
+    image: "/images/cog.png",
   },
   {
     name: "GetLinked Technologies",
     description: "An engaging application focused on task management.",
     demoLink: "",
-    image: '/images/getLinked.png',
+    image: "/images/getLinked.png",
   },
   {
     name: "Tech Alpha Quiz",
     description: "A dynamic and interactive quiz platform.",
     demoLink: "https://quiz-game-app-6qbj.onrender.com",
-    image: '/images/alpha.png',
+    image: "/images/alpha.png",
   },
   {
     name: "Movie Search App",
-    description: "A platform for movie enthusiasts to search and explore movies.",
+    description:
+      "A platform for movie enthusiasts to search and explore movies.",
     demoLink: "https://deluxe-panda-13911d.netlify.app",
-    image: '/images/movie.png',
+    image: "/images/movie.png",
+  },
+  {
+    name: "React Youtube Clone",
+    description:
+      "A platform for movie enthusiasts to search and explore movies.",
+    demoLink: "https://youtube-clone-u9xn.onrender.com",
+    image: "/images/youtube.png",
   },
   {
     name: "NCCF Etinan",
-    description: "A platform that connects members of the National Christian Corpers Fellowship.",
+    description:
+      "A platform that connects members of the National Christian Corpers Fellowship.",
     demoLink: "https://nccf-uuvk.onrender.com",
-    image: '/images/nccf.png',
+    image: "/images/nccf.png",
   },
   {
     name: "Dice Game",
     description: "A fun and interactive game where players roll virtual dice.",
     demoLink: "https://dice-game-app.onrender.com",
-    image: '/images/dice.png',
+    image: "/images/dice.png",
   },
   {
     name: "Jokes and Word App",
-    description: "A simple app that allows users to get random jokes and word definitions.",
+    description:
+      "A simple app that allows users to get random jokes and word definitions.",
     demoLink: "https://joke-app-btkm.onrender.com",
-    image: '/images/jokes.png',
+    image: "/images/jokes.png",
   },
   {
     name: "To-Do List App",
-    description: "A straightforward application that helps users efficiently manage tasks.",
+    description:
+      "A straightforward application that helps users efficiently manage tasks.",
     demoLink: "https://todo-list-app-i5ik.onrender.com",
-    image: '/images/todo.png',
+    image: "/images/todo.png",
   },
 ];
 
