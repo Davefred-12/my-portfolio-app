c5aa4d764bc1a9e661ff2ff5d99c9f1c10c04702
Adeleye Adewale David <adeleyepamilerin9@gmail.com>
2026-01-19 13:44:36 +0100

    updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -46,7 +46,7 @@ const projects = [
     image: "/images/deb.png",
   },
   {
-    name: "Chatterra",
+    name: "Chatlyx",
     description:
       "A real-time messaging platform designed for fast, secure, and meaningful conversations. Chatterra supports one-on-one and group chats, message reactions, media sharing, and online presence indicators. Built with a focus on performance, scalability, and user-friendly interaction, it creates a seamless communication experience across devices.",
     tech: ["React", "Node.js", "Express", "MongoDB", "Socket.IO"],
